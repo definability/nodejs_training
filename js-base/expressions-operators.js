85b00bd56cf63dc929fe2a391fc53cba589c6edd
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-16 13:37:56 +0300

    Updated expressions and operators

--- js-base/expressions-operators.js
+++ js-base/expressions-operators.js
@@ -18,8 +18,8 @@ function codeColumns () {
     var stringToCheck = "Good";
     var GOOD_STRING = "Good";
     var BAD_STRING = "Bad";
-    var isStringCorrect = (stringToCheck == GOOD_STRING ?
-                           1 : stringToCheck == BAD_STRING ?
+    var isStringCorrect = (stringToCheck === GOOD_STRING ?
+                           1 : stringToCheck === BAD_STRING ?
                            1 : -1);
 
     // bad
@@ -28,4 +28,34 @@ function codeColumns () {
     var GOOD_STRING = "Bad";
     var BAD_STRING = "Bad";
     var isStringCorrect = ((stringToCheck == GOOD_STRING || stringToCheck == BAD_STRING) ? 1 : -1);
+}
+
+function increment () {
+    // good
+
+    var i = 1;
+    i++;
+    i *= 3;
+    i++;
+
+    // bad
+
+    var i = 1;
+    i += i+++++i;
+}
+
+function floatComparison () {
+    // good
+
+    var f1 = 1.0;
+    var f2 = Math.sqrt(1.0);
+    var TOLERANCE = Number.EPSILON === undefined ? 1E-6 : Number.EPSILON;
+
+    var areEqual = Math.abs(f1 - f2) < TOLERANCE;
+
+    // bad
+
+    var f1 = 1.0;
+    var f2 = 1.0 * Math.sqrt(1.0);
+    var areEqual = f1 == f2;
 }
