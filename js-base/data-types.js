d010d77a606b03217b086daa58253bf7cb30ac5c
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-17 15:45:42 +0300

    Updated data-types

--- js-base/data-types.js
+++ js-base/data-types.js
@@ -4,8 +4,9 @@ var booleanVar = false;
     numberVar = 123,
     numberNanVar = NaN,
     numberInfVar = Infinity,
-    objectVar = {"num": 1, "str": "1", "bool": true},
-    arrayVar = [1, 2, 3],
+    objectVar = {"num": 1, "str": "1", "bool": true, "array": [1, 2, 3],
+                 "undefined": undefined, "object": {"unity": 1}},
+    arrayVar = [1, "2", {"num", 3}, [4, 5, [6]]],
     undefinedVar = undefined,
     objectNullVar = null,
     objectBlankListVar = [];
