caecb51eb8e2ce917f5d6c8001529c1871de039c
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-17 17:39:31 +0300

    Fixed error in array.js

--- js-base/array.js
+++ js-base/array.js
@@ -1,5 +1,7 @@
 function badTree() {
     var goodTree = [1, 2, [3, 4, [5, 6]]];
-    var badTree [goodTree];
+    var badTree  = [goodTree];
     badTree.push(badTree); // Good luck visiting all children!
 }
+
+
