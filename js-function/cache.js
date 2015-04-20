fc5f1c897dcf20953451ad64fb55e8593812b26e
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-20 15:05:57 +0300

    Removed callbacks

--- js-function/cache.js
+++ js-function/cache.js
@@ -4,51 +4,55 @@ var Cache = (function() {
     };
     var proto = constructor.prototype;
     
-    proto.__check_unique_key = function (key, onError) {
-        if (this.value.hasOwnProperty(key)) {
-            return onError('must be unique');
-        }
+    proto.__validate = function (validation, values) {
+        var disjunction = function (a, b) {
+            return a & b;
+        };
+        return key.map(validation, this).reduce(disjunction);
+    };
+    proto.__check_unique_key = function (key) {
         return !this.value.hasOwnProperty(key);
     };
-    proto.__check_mandatory = function (value, onError) {
-        if (value === undefined) {
-            return onError('is mandatory');
+    proto.__check_mandatory = function (value) {
+        if (!Array.isArray(value)) {
+            return value !== undefined;
+        } else {
+            var disjunction = function (a, b) {
+                return a & b;
+            };
+            return value.map(this.__check_mandatory, this).reduce(disjunction);
         }
-        return value !== undefined;
     };
 
     proto.add = function (key, value) {
-        this.__check_mandatory(key, function(message) {
-            throw new Error('key ' + message);
-        });
-        this.__check_mandatory(value, function(message) {
-            throw new Error('value ' + message);
-        });
-        this.__check_unique_key(key, function(message) {
-            throw new Error('key ' + message);
-        });
-        this.value[key] = value;
+        if (!this.__check_mandatory([key, value])) {
+            throw new Error('Fields `key\' and `value\' are mandatory');
+        } else if (!this.__check_unique_key(key)) {
+            throw new Error('Field `key\' must be unique');
+        } else {
+            this.value[key] = value;
+        }
     };
     proto.update = function (key, value) {
-        this.__check_mandatory(key, function(message) {
-            throw new Error('key ' + message);
-        });
-        this.__check_mandatory(value, function(message) {
-            throw new Error('value ' + message);
-        });
-        this.value[key] = value;
+        if (!this.__check_mandatory([key, value])) {
+            throw new Error('Fields `key\' and `value\' are mandatory');
+        } else {
+            this.value[key] = value;
+        }
     };
     proto.get = function (key) {
-        this.__check_mandatory(key, function(message) {
-            throw new Error('key ' + message);
-        });
-        return this.value[key];
+        if (!this.__check_mandatory(key)) {
+            throw new Error('Field `key\' is mandatory');
+        } else {
+            return this.value[key];
+        }
     };
     proto.delete = function (key) {
-        this.__check_mandatory(key, function(message) {
-            throw new Error('key ' + message);
-        });
-        delete this.value[key];
+        if (!this.__check_mandatory(key)) {
+            throw new Error('Field `key\' is mandatory');
+        } else {
+            delete this.value[key];
+        }
     };
     proto.find = function (query) {
         var isSubstring = function (substring) {
