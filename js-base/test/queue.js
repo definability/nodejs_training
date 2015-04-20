b139dee6bf5267d771dd6f494b8da0e052b5e8f6
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-20 13:18:42 +0300

    Updated Stack and Queue tests

--- js-base/test/queue.js
+++ js-base/test/queue.js
@@ -2,26 +2,27 @@ var assert = require("assert");
 var Queue = require("../array.js").Queue;
 
 describe('Queue', function(){
-    describe('#push()', function(){
+    var q;
+    beforeEach(function() {
+        q = new Queue();
+    });
+    describe('#push(value)', function(){
         it('should return undefined on push', function() {
-            var q = new Queue();
             assert.equal(q.push(null), undefined);
             assert.equal(q.push(0), undefined);
         });
     });
     describe('#pop()', function(){
         it('should return undefined when empty', function() {
-            var q = new Queue();
             assert.equal(q.pop(), undefined);
         });
         it('should return constructor arguments in direct order', function() {
-            var q = new Queue(['a', 'b', 'c']);
+            q = new Queue(['a', 'b', 'c']);
             assert.equal(q.pop(), 'a');
             assert.equal(q.pop(), 'b');
             assert.equal(q.pop(), 'c');
         });
         it('should return first pushed value', function() {
-            var q = new Queue();
             q.push(1);
             q.push(2);
             assert.equal(q.pop(), 1);
--- js-base/test/stack.js
+++ js-base/test/stack.js
@@ -1,27 +1,28 @@
 var assert = require("assert");
 var Stack = require("../array.js").Stack;
 
-describe('Stack', function(){
-    describe('#push()', function(){
+describe('Stack', function() {
+    var s;
+    beforeEach(function() {
+        s = new Stack();
+    });
+    describe('#push(value)', function() {
         it('should return undefined on push', function() {
-            var s = new Stack();
             assert.equal(s.push(null), undefined);
             assert.equal(s.push(0), undefined);
         });
     });
     describe('#pop()', function(){
         it('should return undefined when empty', function() {
-            var s = new Stack();
             assert.equal(s.pop(), undefined);
         });
         it('should return constructor arguments in reversed order', function() {
-            var s = new Stack(['a', 'b', 'c']);
+            s = new Stack(['a', 'b', 'c']);
             assert.equal(s.pop(), 'c');
             assert.equal(s.pop(), 'b');
             assert.equal(s.pop(), 'a');
         });
         it('should return last pushed value', function() {
-            var s = new Stack();
             s.push(1);
             s.push(2);
             assert.equal(s.pop(), 2);
