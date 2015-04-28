5e46c5a7e95b1b0d01a2c47370a5cd2300adc208
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-28 13:02:05 +0300

    Tested GET, POST and DELETE

--- nodejs/chatroom/routes/users.js
+++ nodejs/chatroom/routes/users.js
@@ -3,7 +3,8 @@ var express = require('express'),
     router = express.Router(),
     MongoClient = require('mongodb').MongoClient,
     ObjectId = require('mongodb').ObjectId,
-    Users = require('../models/Users.js');
+    Users = require('../models/Users.js'),
+    debug = require('debug')('users');
 
 var url = 'mongodb://localhost:27017/chatroom',
     connect = function (callback) {
@@ -19,7 +20,7 @@ router.get('/', function(request, response, next) {
         users.connect(db);
         users.get({}, function (err, got) {
             assert.equal(err, null);
-            response.json(got);
+            response.json({success: true, response: got});
         });
     });
 });
@@ -37,22 +38,62 @@ router.get('/:id', function(request, response, next) {
         users.get({_id: userId}, function (err, got) {
             assert.equal(err, null);
             if (got.length == 0) {
-                response.status(404).send('Not found');
+                response.status(404).json({success: false, error: 'Not found'});
             } else {
-                response.json(got);
+                response.json({success: true, response: got});
             }
         });
     });
 });
 
-router.post('/', function(request, response) {
+router.post('/', function(request, response, next) {
     var users = new Users();
     connect(function (db) {
         users.connect(db);
-        users.post([request.body], function (err, result) {
+        try {
+            if (Object.keys(request.body).length == 0) {
+                throw new Error('Wrong');
+            }
+            users.post([request.body], function (err, result) {
+                assert.equal(err, null);
+                users.close();
+                response.json({success: result.result.ok == 1, response: {users: result.ops}});
+            });
+        } catch (e) {
+            response.status(400).json({success: false, error: 'Wrong user data'});
+        }
+    });
+});
+
+router.delete('/', function(request, response, next) {
+    var users = new Users();
+    connect(function (db) {
+        users.connect(db);
+        users.delete(request.body, function (err, result) {
             assert.equal(err, null);
             users.close();
-            response.json(result.result);
+            response.json({success: result.result.ok == 1});
+        });
+    });
+});
+
+router.delete('/:id', function(request, response, next) {
+    connect(function(db) {
+        var users,
+            userId;
+        try {
+            userId = ObjectId(request.params.id);
+        } catch (e) {
+        }
+        users = new Users();
+        users.connect(db);
+        users.delete({_id: userId}, function (err, deleted) {
+            assert.equal(err, null);
+            if (deleted.length == 0) {
+                response.status(404).json({success: false, error: 'Not found'});
+            } else {
+                response.json({success: deleted.result.ok == 1, response: {count: deleted.result.n}});
+            }
         });
     });
 });
--- nodejs/chatroom/test/httpUsers.js
+++ nodejs/chatroom/test/httpUsers.js
@@ -1,6 +1,5 @@
 var assert = require('assert'),
     http = require('http'),
-    querystring = require('querystring'),
     faker = require('faker');
 
 var host = 'localhost',
@@ -9,42 +8,100 @@ var host = 'localhost',
     url = ['http://', host, ':', port, path].join('');
 
 describe.only('HTTP server with Users', function() {
-    it('Should return JSON', function(done) {
-        http.get(url, function (res) {
-            var data = '';
+    var currentUser;
+    var generateRequest = function (method, onEnd, pathAppendix) {
+        if (pathAppendix === undefined) {
+            pathAppendix = '';
+        }
+        var options = {
+            host: host,
+            port: port,
+            path: [path, pathAppendix].join('/'),
+            method: method
+        };
+        var request = http.request(options, function (res) {
+            var chunks = [];
+
             res.on('data', function (chunk) {
-                data += chunk;
+                chunks.push(chunk);
             });
             res.on('end', function() {
+                onEnd(chunks.join(''));
+            });
+        });
+        request.setHeader('Content-Type', 'application/json');
+        return request;
+    };
+    describe('#POST', function() {
+        it('Should post blank user with error', function (done) {
+            var request = generateRequest('POST', function (data) {
+                assert.equal(JSON.parse(data).success, false);
+                done();
+            });
+            request.end();
+        });
+        it('Should post user correctly', function (done) {
+            currentUser = {
+                name: faker.name.findName(),
+                creationDate: Date.now(),
+                email: faker.internet.email(),
+                address: faker.address.streetAddress()
+            };
+            var request = generateRequest('POST', function (data) {
+                dataJSON = JSON.parse(data);
+                assert.equal(dataJSON.success, true);
+                assert.equal(dataJSON.response.users.length, 1);
+                currentUser._id = dataJSON.response.users[0]._id;
+                assert.deepEqual(dataJSON.response.users[0], currentUser);
+                done();
+            });
+            request.write(JSON.stringify(currentUser));
+            request.end();
+        });
+    });
+    describe('#GET', function() {
+        it('Should return JSON', function (done) {
+            var request = generateRequest('GET', function (data) {
                 assert.doesNotThrow(function() { JSON.parse(data); });
                 done();
             });
+            request.end();
         });
-    });
-    it('Should post user correctly', function(done) {
-        var postData = querystring.stringify({
-            name: faker.name.findName(),
-            creationDate: Date.now(),
-            email: faker.internet.email(),
-            address: faker.address.streetAddress()
-        });
-        var postOptions = {
-            host: host,
-            port: port,
-            path: path,
-            method: 'POST',
-        };
-        var postRequest = http.request(postOptions, function (res) {
-            var data = '';
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-            res.on('end', function() {
-                console.log(data);
+        it('Should be success', function (done) {
+            var request = generateRequest('GET', function (data) {
+                assert.equal(JSON.parse(data).success, true);
                 done();
             });
+            request.end();
         });
-        postRequest.write(postData);
-        postRequest.end();
+        it('Should get previously user', function (done) {
+            var request = generateRequest('GET', function (data) {
+                dataJSON = JSON.parse(data);
+                assert.equal(dataJSON.success, true);
+                assert.equal(dataJSON.response.length, 1);
+                assert.deepEqual(dataJSON.response[0], currentUser);
+                done();
+            }, currentUser._id);
+            request.end();
+        });
+    });
+    describe('#DELETE', function() {
+        it('Should delete users got in previous tests without error', function (done) {
+            var request = generateRequest('DELETE', function (data) {
+                dataJSON = JSON.parse(data);
+                assert.equal(dataJSON.success, true);
+                assert.equal(dataJSON.response.count, 1);
+                done();
+            }, currentUser._id);
+            request.end();
+        });
+        it('Deleted user should not exist anymore', function (done) {
+            var request = generateRequest('GET', function (data) {
+                dataJSON = JSON.parse(data);
+                assert.equal(dataJSON.success, false);
+                done();
+            }, currentUser._id);
+            request.end();
+        });
     });
 });
