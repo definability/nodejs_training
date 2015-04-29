a92077a88e5725004cb4c5555b833fcd3bc55275
char-lie <5962274+char-lie@users.noreply.github.com>
2015-04-29 16:23:31 +0300

    Prettified Model

--- nodejs/chatroom/models/Model.js
+++ nodejs/chatroom/models/Model.js
@@ -3,89 +3,95 @@ var _ = require('lodash'),
 
 var MetaModel;
 
-MetaModel = (function() {
-    var metaConstructor = function (schemaInfo) {
-        var constructor = function (res, next) {
-            var database, collection, connected = false;
-            var schema = {
-                name: schemaInfo.name,
-                fields: ['_id']
-            };
-            Array.prototype.push.apply(schema.fields, schemaInfo.fields);
-            Object.freeze(schema);
-            this.connect = function (db) {
-                if (db !== undefined) {
-                    database = db;
-                }
-                collection = database.collection(schema['name']);
-                connected = true;
-            };
-            this.close = function (callback) {
-                database.close(callback);
-                connected = false;
-            };
-            this.isConnected = function() {
-                return connected;
-            };
-            this.getSchema = function() {
-                return schema;
-            };
-            this.getCollection = function() {
-                return collection;
-            }
+var Model = (function() {
+    var constructor = function (schemaInfo) {
+        var database, collection, connected = false;
+        var schema = {
+            name: schemaInfo.name,
+            fields: ['_id']
         };
-        var proto = constructor.prototype;
-        proto.get = function (parameters, callback) {
-            if (parameters === undefined) {
-                parameters = {};
+        Array.prototype.push.apply(schema.fields, schemaInfo.fields);
+        Object.freeze(schema);
+        this.connect = function (db) {
+            if (db !== undefined) {
+                database = db;
             }
-            this.getCollection().find(parameters).toArray(callback);
+            collection = database.collection(schema['name']);
+            connected = true;
         };
-        proto.findById = function (id, callback) {
-            var objectId;
-            try {
-                objectId = ObjectId(id);
-            } catch (e) {
-                callback(e);
-                return;
-            }
-            this.get({_id: objectId}, callback);
+        this.close = function (callback) {
+            database.close(callback);
+            connected = false;
         };
-        proto.processObject = function (newObject) {
-            return _.pick(newObject, this.getSchema().fields);
+        this.isConnected = function() {
+            return connected;
+        };
+        this.getSchema = function() {
+            return schema;
+        };
+        this.getCollection = function() {
+            return collection;
         }
-        proto.post = function (objects, callback) {
-            var error = null;
-            if (!Array.isArray(objects)) {
-                error = new Error('First argument should be an array');
-            } else if (callback === undefined) {
-                error = new Error('Callback field is mandatory');
-            } else {
-                this.getCollection().insert(objects.map(this.processObject, this), callback);
-            }
-            if (error != null) {
-                callback(error);
-            }
-        };
-        proto.delete = function (parameters, callback) {
-            if (parameters === undefined) {
-                parameters = {};
-            }
-            this.getCollection().remove(parameters, callback);
-        };
-        proto.deleteById = function (id, callback) {
-            var objectId;
-            try {
-                objectId = ObjectId(id);
-            } catch (e) {
-                callback(e);
-                return;
-            }
-            this.delete({_id: objectId}, callback);
-        };
-        return constructor;
+    };
+    var proto = constructor.prototype;
+    proto.get = function (parameters, callback) {
+        if (parameters === undefined) {
+            parameters = {};
+        }
+        this.getCollection().find(parameters).toArray(callback);
+    };
+    proto.findById = function (id, callback) {
+        var objectId;
+        try {
+            objectId = ObjectId(id);
+        } catch (e) {
+            callback(e);
+            return;
+        }
+        this.get({_id: objectId}, callback);
+    };
+    proto.processObject = function (newObject) {
+        return _.pick(newObject, this.getSchema().fields);
     }
-    return metaConstructor;
+    proto.post = function (objects, callback) {
+        var error = null;
+        if (!Array.isArray(objects)) {
+            error = new Error('First argument should be an array');
+        } else if (callback === undefined) {
+            error = new Error('Callback field is mandatory');
+        } else {
+            this.getCollection().insert(objects.map(this.processObject, this), callback);
+        }
+        if (error != null) {
+            callback(error);
+        }
+    };
+    proto.delete = function (parameters, callback) {
+        if (parameters === undefined) {
+            parameters = {};
+        }
+        this.getCollection().remove(parameters, callback);
+    };
+    proto.deleteById = function (id, callback) {
+        var objectId;
+        try {
+            objectId = ObjectId(id);
+        } catch (e) {
+            callback(e);
+            return;
+        }
+        this.delete({_id: objectId}, callback);
+    };
+    return constructor;
+})();
+
+MetaModel = (function() {
+    var constructor = function (schemaInfo) {
+        return function() {
+            return new Model(schemaInfo);
+        };
+    };
+    return constructor;
 })();
 
 module.exports = {MetaModel: MetaModel};
